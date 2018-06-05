db99b0defea45c9b58b81140ec58a7694278f3f0
Joe McCann <wjm9@njit.edu>
2018-06-04 22:27:35 -0400

    thing
    test

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -25,23 +25,14 @@
         center: { lat: 40.5000283, lng: -74.3068167 },
         /* eslint-disable */
         markers: [
-          { position: { lat: 40.4087405, lng: -74.2505561 },
-            icon: (new google.maps.MarkerImage('http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|' + 'FE7569',
-              new google.maps.Size(21, 34),
-              new google.maps.Point(0, 0),
-              new google.maps.Point(10, 34)))
+          { position: { lat: 40.4087405, lng: -75.2505561 },
+            icon: (new google.maps.MarkerImage('http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|' + 'FE7569'))
           },
           { position: { lat: 40.5913161, lng: -74.3630773 },
-            icon: (new google.maps.MarkerImage('http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|' + 'FE7569',
-              new google.maps.Size(21, 34),
-              new google.maps.Point(0, 0),
-              new google.maps.Point(10, 34)))
+            icon: (new google.maps.MarkerImage('http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|' + 'FE7569'))
           },
           { position: { lat: 40.5000283, lng: -74.3068167 },
-            icon: (new google.maps.MarkerImage('http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|' + '3AED0E',
-              new google.maps.Size(21, 34),
-              new google.maps.Point(0, 0),
-              new google.maps.Point(10, 34)))
+            icon: (new google.maps.MarkerImage('http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|' + '3AED0E'))
           }
         ]
         /* eslint-enable */
