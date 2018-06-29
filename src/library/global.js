c725df605341148d0e5ae11e0e4162feab062df8
iambillmccann <bill.mccann@gmail.com>
2018-06-29 16:11:43 -0400

    Issue #18

--- src/library/global.js
+++ src/library/global.js
@@ -4,7 +4,7 @@
 //
 
 export default class Functions {
-  idGenerator () { return 'LETS-HANG|' + Math.floor(Math.random() * 1000000).toString() }
+  idGenerator () { return 'LETSHANG|' + Math.floor(Math.random() * 1000000).toString() }
   getFullName (person) { return person.firstName + ' ' + person.lastName }
   getInitials (person) { return person.firstName[0].toUpperCase() + person.lastName[0].toUpperCase() }
 }
