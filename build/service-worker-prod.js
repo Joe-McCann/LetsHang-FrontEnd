6a9e6d758996464ddee2370dbee70cb45909ee88
iambillmccann <bill.mccann@gmail.com>
2019-01-01 16:02:18 -0500

    Testing by trying app.yaml

--- build/service-worker-prod.js
+++ build/service-worker-prod.js
@@ -24,7 +24,7 @@
     }
     
     // const serviceWorkerURL = '/service-worker.js'
-    const serviceWorkerURL = 'service-worker.js'
+    const serviceWorkerURL = 'app.yaml'
     const scope = './'
 
     // Trouble shooting logs
